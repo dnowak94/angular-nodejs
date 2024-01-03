e61b950f883541a02ae8603fec523322c95c2095
Dominik Nowak <dnowak94@live.de>
2024-01-03 17:12:36 +0100

    add Dockerfiles and docker-compose file

---
+++ tutorial-heroes/db/sql/02-heroes.sql
@@ -0,0 +1,12 @@
+USE hero;
+INSERT INTO heroes (name, createdAt, updatedAt)
+VALUES
+('Dr. Nice',NOW(), NOW()),
+('Bombasto',NOW(), NOW()),
+('Celeritas',NOW(), NOW()),
+('Magneta',NOW(), NOW()),
+('RubberMan',NOW(), NOW()),
+('Dynama',NOW(), NOW()),
+('Dr. IQ',NOW(), NOW()),
+('Magma',NOW(), NOW()),
+('Tornado',NOW(), NOW());
