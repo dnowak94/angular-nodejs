b5848ef431a21b58dc3c533a6261b19b6b301219
Dominik Nowak <dnowak94@live.de>
2024-01-05 18:33:15 +0100

    fixed links in navigation bar and backend url

--- webshop/src/app/app.component.ts
+++ webshop/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 import { NgbNav, NgbNavItem, NgbNavLink, NgbNavOutlet } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgbNav, NgbNavItem, NgbNavLink, NgbNavOutlet],
+  imports: [CommonModule, RouterOutlet, NgbNav, NgbNavItem, NgbNavLink, NgbNavOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
