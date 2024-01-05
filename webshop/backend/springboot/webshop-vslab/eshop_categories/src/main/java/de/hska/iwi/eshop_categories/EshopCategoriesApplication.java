084969ba3e2dc236b5eb7810c52adfe275b6ef66
Dominik Nowak <dnowak94@live.de>
2024-01-05 18:17:20 +0100

    added CORS in backend services

--- webshop/backend/springboot/webshop-vslab/eshop_categories/src/main/java/de/hska/iwi/eshop_categories/EshopCategoriesApplication.java
+++ webshop/backend/springboot/webshop-vslab/eshop_categories/src/main/java/de/hska/iwi/eshop_categories/EshopCategoriesApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Configuration
@@ -20,4 +23,13 @@ public class EshopCategoriesApplication {
 		SpringApplication.run(EshopCategoriesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/categories").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
--- webshop/backend/springboot/webshop-vslab/eshop_products/src/main/java/de/hska/iwi/eshop_products/EshopProductsApplication.java
+++ webshop/backend/springboot/webshop-vslab/eshop_products/src/main/java/de/hska/iwi/eshop_products/EshopProductsApplication.java
@@ -3,9 +3,12 @@ package de.hska.iwi.eshop_products;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Configuration
@@ -18,4 +21,13 @@ public class EshopProductsApplication {
 		SpringApplication.run(EshopProductsApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/products").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
